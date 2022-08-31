07a68c666ef15430c75c8004aead2c90232234d4
YaroslavAntoniuk <yaroslav.antoniuk@binary-studio.com>
2022-09-01 00:14:20 +0300

    Added cors

--- server/app.js
+++ server/app.js
@@ -3,11 +3,14 @@ const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('../schemas/schema');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const app = express();
 const PORT = 3005;
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.jfhherq.mongodb.net/graphql?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
   schema,
