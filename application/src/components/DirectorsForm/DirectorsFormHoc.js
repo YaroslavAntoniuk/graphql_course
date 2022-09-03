50f4347c0c4306fcf1cb3df98338e942d7e6af97
YaroslavAntoniuk <yaroslav.antoniuk@binary-studio.com>
2022-09-03 16:01:33 +0300

    Added add director mutation & hoc

--- application/src/components/DirectorsForm/DirectorsFormHoc.js
+++ application/src/components/DirectorsForm/DirectorsFormHoc.js
@@ -1,6 +1,19 @@
 import { withStyles } from '@material-ui/core/styles';
+import { graphql } from 'react-apollo';
 import { compose } from 'recompose';
+import { directorsQuery } from '../DirectorsTable/queries';
+import { addDirectorMutation } from './mutations';
 
 import { styles } from './styles';
 
-export default compose(withStyles(styles));
+const withGraphQlAdd = graphql(addDirectorMutation, {
+  props: ({ mutate }) => ({
+    addDirector: (director) =>
+      mutate({
+        variables: director,
+        refetchQueries: [{ query: directorsQuery }],
+      }),
+  }),
+});
+
+export default compose(withStyles(styles), withGraphQlAdd);
