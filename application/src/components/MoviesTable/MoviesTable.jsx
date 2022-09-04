33f5984ce11a19c244a60429c4b0f66127d0eb9e
YaroslavAntoniuk <yaroslav.antoniuk@binary-studio.com>
2022-09-04 19:26:47 +0300

    Added movies search by name

--- application/src/components/MoviesTable/MoviesTable.jsx
+++ application/src/components/MoviesTable/MoviesTable.jsx
@@ -14,6 +14,7 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import CreateIcon from '@material-ui/icons/Create';
 
 import MoviesDialog from '../MoviesDialog/MoviesDialog';
+import MoviesSearch from '../MoviesSearch/MoviesSearch';
 
 import withHocs from './MoviesTableHoc';
 
@@ -21,6 +22,23 @@ class MoviesTable extends React.Component {
   state = {
     anchorEl: null,
     openDialog: false,
+    name: '',
+  };
+
+  handleChange = name => (event) => {
+    this.setState({ [name]: event.target.value });
+  };
+
+  handleSearch = (e) => {
+    const { data } = this.props;
+    const { name } = this.state;
+
+    if(e.charCode === 13) {
+      data.fetchMore({
+        variables: { name },
+        updateQuery: (previousResult, { fetchMoreResult }) => fetchMoreResult,
+      });
+    }
   };
 
   handleDialogOpen = () => {
@@ -52,13 +70,16 @@ class MoviesTable extends React.Component {
   };
 
   render() {
-    const { anchorEl, openDialog, data: activeElem = {} } = this.state;
+    const { anchorEl, openDialog, data: activeElem = {}, name } = this.state;
 
     const { classes, data } = this.props;
     const { movies = [] } = data;
 
     return (
       <>
+        <Paper>
+          <MoviesSearch name={name} handleChange={this.handleChange} handleSearch={this.handleSearch} />
+        </Paper>
         <MoviesDialog
           open={openDialog}
           handleClose={this.handleDialogClose}
@@ -77,7 +98,7 @@ class MoviesTable extends React.Component {
               </TableRow>
             </TableHead>
             <TableBody>
-              {movies.map(movie => {
+              {movies.map((movie) => {
                 return (
                   <TableRow key={movie.id}>
                     <TableCell component="th" scope="row">
@@ -85,7 +106,9 @@ class MoviesTable extends React.Component {
                     </TableCell>
                     <TableCell>{movie.genre}</TableCell>
                     <TableCell align="right">{movie.rate}</TableCell>
-                    <TableCell>{(movie.director && movie.director.name) || 'No Director'}</TableCell>
+                    <TableCell>
+                      {(movie.director && movie.director.name) || 'No Director'}
+                    </TableCell>
                     <TableCell>
                       <Checkbox checked={movie.watched} disabled />
                     </TableCell>
