44657bb7844357b11ee98039a9ba34ba0928b092
YaroslavAntoniuk <yaroslav.antoniuk@binary-studio.com>
2022-09-04 18:27:36 +0300

    Added update functionality for movies

--- application/src/components/MoviesForm/MoviesFormHoc.js
+++ application/src/components/MoviesForm/MoviesFormHoc.js
@@ -2,24 +2,35 @@ import { withStyles } from '@material-ui/core/styles';
 import { graphql } from 'react-apollo';
 import { compose } from 'recompose';
 import { moviesQuery } from '../MoviesTable/queries';
-import { addMovieMutation } from './mutations';
+import { addMovieMutation, updateMovieMutation } from './mutations';
 import { directorsQuery } from './queries';
 import { directorsQuery as allDirectors } from '../DirectorsTable/queries';
 
 import { styles } from './styles';
 
-const withGraphQlAdd = graphql(addMovieMutation, {
-  props: ({ mutate }) => ({
-    addMovie: (movie) =>
-      mutate({
-        variables: movie,
-        refetchQueries: [{ query: moviesQuery }, { query: allDirectors}],
-      }),
+const withGraphQL = compose(
+  graphql(addMovieMutation, {
+    props: ({ mutate }) => ({
+      addMovie: (movie) =>
+        mutate({
+          variables: movie,
+          refetchQueries: [{ query: moviesQuery }, { query: allDirectors }],
+        }),
+    }),
   }),
-});
+  graphql(updateMovieMutation, {
+    props: ({ mutate }) => ({
+      updateMovie: (movie) =>
+        mutate({
+          variables: movie,
+          refetchQueries: [{ query: moviesQuery }, { query: allDirectors }],
+        }),
+    }),
+  }),
+);
 
 export default compose(
   withStyles(styles),
-  withGraphQlAdd,
+  withGraphQL,
   graphql(directorsQuery),
 );
